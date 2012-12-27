e8fa5b2ee7be65e2a6d63fd03dde3add7f72fa8a
Grafe <flingelfrank@hotmail.com>
2012-12-27 16:51:23 +0100

    Added loot inventory name

--- DungeonsXL/src/com/dre/dungeonsxl/DLootInventory.java
+++ DungeonsXL/src/com/dre/dungeonsxl/DLootInventory.java
@@ -10,27 +10,27 @@ import org.bukkit.inventory.ItemStack;
 
 public class DLootInventory {
 	public static CopyOnWriteArrayList<DLootInventory> LootInventorys=new CopyOnWriteArrayList<DLootInventory>();
-	
+
 	public Inventory inventory;
 	public InventoryView inventoryView;
 	public Player player;
 
 	public long time;
-	
+
 	public DLootInventory(Player player, ItemStack[] istacks){
 		LootInventorys.add(this);
-		
-		this.inventory = Bukkit.createInventory(player, 54);
+
+		this.inventory = Bukkit.createInventory(player, 54, "Belohnungen");
 		for(ItemStack istack:istacks){
 			if(istack!=null){
 				this.inventory.addItem(istack);
 			}
 		}
 		this.player=player;
-		
-		
+
+
 	}
-	
+
 
 	public static DLootInventory get(Player player) {
 		for(DLootInventory inventory:LootInventorys){
