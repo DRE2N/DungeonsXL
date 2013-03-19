c96225bf574948a6a84e5a136e7509ce309e4130
Grafe <flingelfrank@hotmail.com>
2013-03-19 17:54:12 +0100

    Created new Sign classes
    Signed-off-by: Grafe <flingelfrank@hotmail.com>

---
+++ src/com/dre/dungeonsxl/game/DSignType.java
@@ -0,0 +1,13 @@
+package com.dre.dungeonsxl.game;
+
+public class DSignType {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
