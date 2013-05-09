cb663d787e652f56feb85e5802524f2c10718fc6
Grafe <flingelfrank@hotmail.com>
2013-05-09 16:34:10 +0200

    Changed SIGNChest to recognise top and bottom chest
    Signed-off-by: Grafe <flingelfrank@hotmail.com>

--- src/com/dre/dungeonsxl/signs/SIGNChest.java
+++ src/com/dre/dungeonsxl/signs/SIGNChest.java
@@ -25,17 +25,15 @@ public class SIGNChest extends DSign {
 	@Override
 	public void onInit() {
 		if (sign.getTypeId() == 63) {
-			for (int x = -1; x <= 1; x++) {
-				if (sign.getBlock().getRelative(x, 0, 0).getTypeId() == 54) {
-					new GameChest(sign.getBlock().getRelative(x, 0, 0), gworld);
+			for (int i = -1; i <= 1; i++) {
+				if (sign.getBlock().getRelative(i, 0, 0).getTypeId() == 54) {
+					new GameChest(sign.getBlock().getRelative(i, 0, 0), gworld);
 				}
-			}
-
-			for (int z = -1; z <= 1; z++) {
-				if (sign.getBlock().getRelative(0, 0, z).getTypeId() == 54) {
-					if (sign.getBlock().getRelative(0, 0, z) != null) {
-						new GameChest(sign.getBlock().getRelative(0, 0, z), gworld);
-					}
+				if (sign.getBlock().getRelative(0, 0, i).getTypeId() == 54) {
+					new GameChest(sign.getBlock().getRelative(0, 0, i), gworld);
+				}
+				if (sign.getBlock().getRelative(0, i, 0).getTypeId() == 54) {
+					new GameChest(sign.getBlock().getRelative(0, i, 0), gworld);
 				}
 			}
 		}
