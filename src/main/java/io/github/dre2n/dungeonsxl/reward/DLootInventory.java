7cbe7f44bb402079a51fd2c2c7b5908aa53ad92c
Daniel Saukel <sa.da@unitybox.de>
2016-04-29 22:01:25 +0200

    Move DLootInventory to reward package

--- src/main/java/io/github/dre2n/dungeonsxl/reward/DLootInventory.java
+++ src/main/java/io/github/dre2n/dungeonsxl/reward/DLootInventory.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package io.github.dre2n.dungeonsxl.dungeon;
+package io.github.dre2n.dungeonsxl.reward;
 
 import io.github.dre2n.dungeonsxl.DungeonsXL;
 import io.github.dre2n.dungeonsxl.config.DMessages;
