4efa206bba95081a11d2f0568298df55dea98c9e
Daniel Saukel <sa.da@unitybox.de>
2016-12-15 15:33:07 +0100

    Fixed RP-API shading

--- core/src/main/java/io/github/dre2n/dungeonsxl/config/MainConfig.java
+++ core/src/main/java/io/github/dre2n/dungeonsxl/config/MainConfig.java
@@ -30,8 +30,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.configuration.ConfigurationSection;
 
 /**
