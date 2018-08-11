c94d2946ac47e9022838050a8d3d078dc8dd04a9
Daniel Saukel <sa.da@unitybox.de>
2018-08-11 04:25:23 +0200

    Add keyItems requirement; resolves #364

---
+++ src/main/java/de/erethon/dungeonsxl/requirement/KeyItemsRequirement.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2012-2018 Frank Baumann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.erethon.dungeonsxl.requirement;
+
+import de.erethon.caliburn.CaliburnAPI;
+import de.erethon.caliburn.item.ExItem;
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * @author Daniel Saukel
+ */
+public class KeyItemsRequirement extends Requirement {
+
+    CaliburnAPI caliburn = CaliburnAPI.getInstance();
+
+    private RequirementType type = RequirementTypeDefault.KEY_ITEMS;
+
+    private List<ExItem> keyItems;
+
+    /* Getters and setters */
+    /**
+     * @return the forbidden items
+     */
+    public List<ExItem> getKeyItems() {
+        return keyItems;
+    }
+
+    @Override
+    public RequirementType getType() {
+        return type;
+    }
+
+    /* Actions */
+    @Override
+    public void setup(ConfigurationSection config) {
+        keyItems = caliburn.deserializeExItemList(config, "keyItems");
+    }
+
+    @Override
+    public boolean check(Player player) {
+        List<ExItem> keyItems = new ArrayList<>(this.keyItems);
+        for (ItemStack item : player.getInventory().getStorageContents()) {
+            keyItems.remove(caliburn.getExItem(item));
+        }
+        return keyItems.isEmpty();
+    }
+
+    @Override
+    public void demand(Player player) {
+    }
+
+}
