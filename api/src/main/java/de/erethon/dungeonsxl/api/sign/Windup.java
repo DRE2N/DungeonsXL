b1342a8d76bf5060055c4d119aaad3041469505e
Daniel Saukel <sa.da@unitybox.de>
2020-02-01 22:19:59 +0100

    Add Javadoc comments, n&k to windup signs

--- api/src/main/java/de/erethon/dungeonsxl/api/sign/Windup.java
+++ api/src/main/java/de/erethon/dungeonsxl/api/sign/Windup.java
@@ -19,23 +19,41 @@ import de.erethon.dungeonsxl.api.world.GameWorld;
 import org.bukkit.block.Sign;
 
 /**
- * A sign with an attached task that does actions in a set interval <i>n</i> times, like a mob sign that spawns <i>n</i> mobs. It is similar to a {@link Rocker}
- * as it expires (=is deactivated).
+ * A sign with an attached task that does actions in a set interval {@link #n} times, like a mob sign that spawns {@link #n} mobs. It is similar to a
+ * {@link Rocker} as it expires (=is deactivated).
  *
  * @author Daniel Saukel
  */
 public abstract class Windup extends Deactivatable {
 
     protected double interval = -1;
+    /**
+     * How many times the task is supposed to be executed (unless it is cancelled).
+     */
+    protected int n;
+    /**
+     * How many times the task has been executed.
+     */
+    protected int k;
 
     protected Windup(DungeonsAPI api, Sign sign, String[] lines, GameWorld gameWorld) {
         super(api, sign, lines, gameWorld);
     }
 
+    /**
+     * Returns the task interval in seconds.
+     *
+     * @return the task interval
+     */
     public double getIntervalSeconds() {
         return interval;
     }
 
+    /**
+     * Returns the task interval in seconds.
+     *
+     * @return the task interval
+     */
     public long getIntervalTicks() {
         return (long) (interval * 20L);
     }
