74f3829282a1d885e0f7b0560af829266a4192c9
Daniel Saukel <Sataniel98@users.noreply.github.com>
2020-06-24 00:31:51 +0200

    Fix kickAllPlayers() NPE

--- core/src/main/java/de/erethon/dungeonsxl/world/DInstanceWorld.java
+++ core/src/main/java/de/erethon/dungeonsxl/world/DInstanceWorld.java
@@ -170,7 +170,9 @@ public abstract class DInstanceWorld implements InstanceWorld {
     public void kickAllPlayers() {
         getPlayers().forEach(p -> p.leave());
         // Players who shouldn't be in the dungeon but still are for some reason
-        getWorld().getPlayers().forEach(p -> PlayerUtil.secureTeleport(p, Bukkit.getWorlds().get(0).getSpawnLocation()));
+        if (world != null) {
+            getWorld().getPlayers().forEach(p -> PlayerUtil.secureTeleport(p, Bukkit.getWorlds().get(0).getSpawnLocation()));
+        }
     }
 
     /**
