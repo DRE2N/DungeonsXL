00f897fbc372952f7eaaed542652b14642081d35
Sataniel98 <sa.da@unitybox.de>
2026-07-22 05:52:05 +0200

    Force wrapper to use Maven 3.9.16 (3.10.0-rc-1 fails to compile)

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-java -jar mvnbt.jar
+java -jar mvnbt.jar -v 3.9.16
 pause
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar mvnbt.jar
+java -jar mvnbt.jar -v 3.9.16
 read -rp "Press any key to continue..." -n 1
